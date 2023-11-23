2384679bf08d6924c03a221feafbe31ace390d0c
Voron V. S. <vitalij.voron@gmail.com>
2023-11-23 15:10:45 +0300

    start 2

--- src/main/java/Package1/Task1.java
+++ src/main/java/Package1/Task1.java
@@ -4,5 +4,6 @@ public class Task1 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("hello2");
+        System.out.println("tms");
     }
 }
