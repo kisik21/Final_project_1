1d87c0080fe33927db8e2b17a3c7be22a077231c
kisik21 <vitalij.voron@gmail.com>
2023-11-26 22:41:20 +0300

    almost all

--- src/main/java/Package1/AccountTransfer.java
+++ src/main/java/Package1/AccountTransfer.java
@@ -4,13 +4,14 @@ import java.io.*;
 import java.nio.file.*;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AccountTransfer {
-
     private static final String INPUT_DIR = "src/main/resources/input";
     private static final String ARCHIVE_DIR = "src/main/resources/archive";
     private static final String REPORT_FILE = "src/main/resources/report/report.txt";
-
+    private static final String INFO_FILE = "src/main/resources/accounts&sums/accounts&sums.txt";
     public static void main(String[] args) {
 
         Scanner in=new Scanner(System.in);
@@ -35,21 +36,19 @@ public class AccountTransfer {
     private static void parseFiles() {
         List<Path> paths = getAllFilesInDirectory(INPUT_DIR);
         List<String> report = new ArrayList<>();
-
         for (Path path : paths) {
             if (isValidFile(path)) {
                 try {
-                    processFile(path);
-                    report.add(getReportEntry(path, "successfully processed"));
+                    processFile(path, report);
+                    report.add(getReportEntry(path, "file successfully processed"));
                     moveFileToArchive(path);
                 } catch (Exception e) {
                     report.add(getReportEntry(path, "error in processing time: " + e.getMessage()));
                 }
             } else {
-                report.add(getReportEntry(path, "invalid format, skipped"));
+                report.add(getReportEntry(path, "invalid format, file skipped"));
             }
         }
-
         writeReportToFile(report);
     }
 
@@ -64,8 +63,7 @@ public class AccountTransfer {
 
     private static List<Path> getAllFilesInDirectory(String directory) {
         List<Path> paths = new ArrayList<>();
-        try {
-            DirectoryStream<Path> dirStream = Files.newDirectoryStream(Paths.get(directory));
+        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(Paths.get(directory))) {
             for (Path path : dirStream) {
                 paths.add(path);
             }
@@ -80,32 +78,61 @@ public class AccountTransfer {
         return fileName.endsWith(".txt");
     }
 
-    private static void processFile(Path path) throws IOException {
+    private static void processFile(Path path, List<String> report) throws IOException {
         List<String> lines = Files.readAllLines(path);
         for (String line : lines) {
             String[] parts = line.split("\\s+");
             String fromAccount = parts[0];
             String toAccount = parts[1];
             int amount = Integer.parseInt(parts[2]);
-
             // Check account numbers and amounts
             if (amount < 0 || !isValidAccountNumber(fromAccount) || !isValidAccountNumber(toAccount)) {
                 throw new IllegalArgumentException("Invalid transfer");
             }
-
             // Update account amounts
-            updateAccountAmount(fromAccount, -amount);
-            updateAccountAmount(toAccount, amount);
+            String str=updateAccountAmount(fromAccount, -amount);
+            report.add(getReportEntry(path, "transfer from " + fromAccount + " to " + toAccount +" " + amount + " " + str ));
+            if (str.equals("successfully processed")) {
+                updateAccountAmount(toAccount, amount);
+            }
         }
     }
 
     private static boolean isValidAccountNumber(String accountNumber) {
-        // Add your validation logic here
-        return true;
+        Pattern pattern = Pattern.compile("[0-9]{5}+-[0-9]{5}");
+        Matcher matcher = pattern.matcher(accountNumber);
+        return matcher.matches();
     }
 
-    private static void updateAccountAmount(String accountNumber, int amount) {
-        // Update account amount in your data model
+    private static String updateAccountAmount(String accountNumber, int amount) {
+        try {
+            File file = new File(INFO_FILE);
+            Scanner fileScanner = new Scanner(file);
+            StringBuilder sb = new StringBuilder();
+            while (fileScanner.hasNextLine()) {
+
+                String line = fileScanner.nextLine();
+                String[] parts = line.split(" ");
+                if (parts[0].equals(accountNumber)) {
+                    int newSum=Integer.parseInt(parts[1])+amount;
+                    if (newSum<0) {
+                        return "not enough money";
+                    }
+                    parts[1] = Integer.toString(newSum);
+                    line = parts[0] + " " + parts[1];
+                }
+                sb.append(line).append("\n");
+            }
+            fileScanner.close();
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(String.valueOf(sb));
+            fileWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found.");
+        } catch (IOException e) {
+            System.out.println("Error: IOException.");
+        }
+        return "successfully processed";
     }
 
     private static void moveFileToArchive(Path path) {
@@ -130,5 +157,3 @@ public class AccountTransfer {
         }
     }
 }
-
-
--- src/main/java/Package2/Task2.java
+++
@@ -1,7 +0,0 @@
-package Package2;
-
-public class Task2 {
-    public static void main(String[] args) {
-        System.out.println("b");
-    }
-}
